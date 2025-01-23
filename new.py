852854f45addbd1e7f8df9e1cfa399dd2d064483
ArunKaviJS <shanarun53787@gmail.com>
2025-01-23 20:10:16 +0530

    modify file

--- new.py
+++ new.py
@@ -1,15 +1,5 @@
 import pandas as pd
 import streamlit as st
-import mysql.connector
-
-
-mydb=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='Jsa.5378724253@'
-)
-arun=mydb.cursor()
-st.write('connected')
 
 path='s3://multiple-disease-s3/Kidney.csv'
 
