4bd61977f37b5d0a71ac9d1594944e2b997ae894
ArunKaviJS <shanarun53787@gmail.com>
2025-01-25 16:04:59 +0530

    Add

--- new.py
+++ new.py
@@ -6,11 +6,11 @@ from io import StringIO
 # S3 file details
 st.title('S3 Data Extract')
 bucket_name = 'multiple-disease-s3'
-path = 'parkinsons.csv'
-def get_s3_file(bucket_name, path):
+path1 = 'Kidney.csv'
+def get_s3_file(bucket_name, path1):
     try:
         s3 = boto3.client('s3')  # No explicit credentials needed if IAM role is used
-        response = s3.get_object(Bucket=bucket_name, Key=path)
+        response = s3.get_object(Bucket=bucket_name, Key=path1)
         data = response['Body'].read().decode('utf-8')
         return data
     except Exception as e:
@@ -19,13 +19,30 @@ def get_s3_file(bucket_name, path):
 with st.sidebar:
    select=st.selectbox('select',['nav','arun'])
 if select=='nav':
- file = get_s3_file(bucket_name, path)
- if file:
+ file1 = get_s3_file(bucket_name, path1)
+ if file1:
  
-  df = pd.read_csv(StringIO(file))
-  st.dataframe(df)
+  df1 = pd.read_csv(StringIO(file1))
+  st.dataframe(df1)
 
-# Streamlit App
+path2 = 'liver.csv'
+def get_s3_file(bucket_name, path2):
+    try:
+        s3 = boto3.client('s3')  # No explicit credentials needed if IAM role is used
+        response = s3.get_object(Bucket=bucket_name, Key=path2)
+        data = response['Body'].read().decode('utf-8')
+        return data
+    except Exception as e:
+        st.error(f"Error accessing S3 file: {e}")
+        return None
+with st.sidebar:
+   select=st.selectbox('select',['nav','arun'])
+if select=='nav':
+ file2 = get_s3_file(bucket_name, path2)
+ if file2:
+ 
+  df2 = pd.read_csv(StringIO(file2))
+  st.dataframe(df2)
 
 
 
